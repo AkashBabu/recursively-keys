b4165d7d9ea77911d8e8188edf91b2a7d6c97dda
Akash Babu V <001akashbabu@gmail.com>
2016-07-17 15:59:37 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+var getKeys = function(object, options, cb){
+	var keys, first = true;
+	if(first){
+		keys = [];
+		first = false;
+		if(arguments.length > 1 && arguments.length < 4){
+			if((object && (options instanceof Function)) && arguments.length == 2){
+				cb = options;
+				options = {recursive : false};
+			}else if((object && options && (cb instanceof Function))){
+
+			}else{
+				throw new Error('Invalid args for getKeys');
+			}
+		}else{
+			throw new Error('Invalid args for getKeys');
+		}
+	}
+	
+	if(object instanceof Object){
+		if(!object.length){
+			if(options.recursive){
+				for(var key in object){
+					keys.push(key);
+					if(object[key] instanceof Object){
+						getKeys(object[key], options, function(err, obtainedKeys){
+							if(!err && (obtainedKeys.length > 0)){
+								keys = keys.concat(obtainedKeys);
+							}
+
+						})
+					}
+				}
+				first = true;
+				cb(null, keys);
+				return;
+			}else{
+				cb(null, Object.keys(object));
+				return;
+			}
+			cb(null, keys);
+			return;
+		}else{
+			cb('Array input is not allowed', null);
+			return;
+		}
+	}else{
+		cb('Object is null', keys);
+		return;
+	}
+}
+
+module.exports = getKeys;
---
+++ test/test-keys.js
@@ -0,0 +1,143 @@
+
+
+var getKeys = require('/home/aakash/Documents/Nodejs/lib/recursive-keys/index.js')
+var assert = require('chai').assert;
+
+var options = {recursive : true};
+
+describe('recursive-keys-test', function(){
+	before(function(){
+		console.log('Begining the test now');
+	});
+	
+	it('Test for null', function(){
+		assert.throws(function(){
+			getKeys(null, function(err, keys){
+		})}, 'Invalid args');
+	});
+
+	it('Test for Undefined', function(){
+		var object;
+		assert.throws(function(){
+			getKeys(object, function(err, keys){
+		})}, 'Invalid args');
+	});
+	
+	it('Test for Arrays', function(){
+		var arr = [1,2,3,4];
+		var myKeys = [];
+		getKeys(arr, function(err, keys){
+			myKeys = keys;
+		});
+		assert.isNull(myKeys, 'Result for arrays is not null')
+	});
+	
+	it('Test for Object', function(){
+		var object = {a : 1, b : 2};
+		var myKeys = ['a', 'b'];
+		var obtainedKeys = [];
+		getKeys(object, function(err, keys){
+			obtainedKeys = keys;
+		});
+		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
+	});
+	
+	it('Test for Object with inner Objects', function(){
+		var object = {a : 1, b : 2, c: { l : 4}};
+		var myKeys = ['a', 'b', 'c'];
+		var obtainedKeys = [];
+		getKeys(object, function(err, keys){
+			obtainedKeys = keys;
+		});
+		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
+	});
+	
+	it('Test for Object with Array', function(){
+		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4}};
+		var myKeys = ['a', 'b', 'c'];
+		var obtainedKeys = [];
+		getKeys(object, function(err, keys){
+			obtainedKeys = keys;
+		});
+		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
+	});
+	
+	it('Test for Object with inner Object with Array', function(){
+		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4, m : {x : [1,32,45], y : 6}}};
+		var myKeys = ['a', 'b', 'c'];
+		var obtainedKeys = [];
+		getKeys(object, function(err, keys){
+			obtainedKeys = keys;
+		});
+		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
+	});
+	
+	it('Test with different Order of parameter', function(){
+		assert.throws(function(){
+			getKeys(function(){}, null)}, 'Invalid args');
+	});
+	
+	it('Test with 4 parameter', function(){
+		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4, m : {x : [1,32,45], y : 6}}};
+		var myKeys = ['a', 'b', 'c', 'l', 'm', 'x', 'y'];
+		var obtainedKeys = [];
+		assert.throws(function(){
+				getKeys(object, options, function(err, keys){
+					obtainedKeys = keys;
+				}, null)
+			}, 'Invalid args'
+		)
+	});
+	
+	it('Test with options and callback interchanged', function(){
+		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4, m : {x : [1,32,45], y : 6}}};
+		var myKeys = ['a', 'b', 'c', 'l', 'm', 'x', 'y'];
+		var obtainedKeys = [];
+		assert.throws(function(){
+				getKeys(object, function(err, keys){
+					obtainedKeys = keys;
+				}, options)
+			}, 'Invalid args'
+		)
+	});
+	
+	it('Test with recursive == true', function(){
+		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4, m : {x : [1,32,45], y : 6}}};
+		var myKeys = ['a', 'b', 'c', 'l', 'm', 'x', 'y'];
+		var obtainedKeys = [];
+		getKeys(object, options, function(err, keys){
+			obtainedKeys = keys;
+		});
+		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
+	});
+	
+	it('Test with no callback function as parameter', function(){
+		var object = {a : 1};
+		assert.throws(function(){
+				getKeys(object, options)
+			},'Invalid args'
+		)
+	});
+	
+	after(function(){
+		console.log('Testing has been completed');
+	});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
