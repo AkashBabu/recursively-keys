7e4dd50af9e907df1f8b3bfcb08609898dc6a142
AkashBabu <001akashbabu@gmail.com>
2016-07-27 23:44:38 +0530

    Added support for Objects within Array

--- test/test-keys.js
+++ test/test-keys.js
@@ -1,80 +1,69 @@
 
+
+
+
+
+
+
+
  
 var getKeys = require('../index.js')
 var assert = require('chai').assert;
 
 var options = {recursive : true};
 
-describe('recursive-keys-test', function(){
+describe('Recursive-Keys-Test : ', function(){
 	before(function(){
 		console.log('Begining the test now');
 	});
 	
 	it('Test for null', function(){
 		assert.throws(function(){
-			getKeys(null, function(err, keys){
-		})}, 'Invalid args');
+				getKeys(null, options);
+		}, 'Error in Arguments Passed for getKeys()');
 	});
 
 	it('Test for Undefined', function(){
 		var object;
 		assert.throws(function(){
-			getKeys(object, function(err, keys){
-		})}, 'Invalid args');
+			getKeys(object);
+		}, 'Error in Arguments Passed for getKeys()');
 	});
 	
 	it('Test for Arrays', function(){
 		var arr = [1,2,3,4];
 		var myKeys = [];
-		getKeys(arr, function(err, keys){
-			myKeys = keys;
-		});
-		assert.isNull(myKeys, 'Result for arrays is not null')
+		assert.throws(function(){
+			getKeys(arr);
+		}, 'Error in Arguments Passed for getKeys()');
 	});
 	
 	it('Test for Object', function(){
 		var object = {a : 1, b : 2};
 		var myKeys = ['a', 'b'];
-		var obtainedKeys = [];
-		getKeys(object, function(err, keys){
-			obtainedKeys = keys;
-		});
+		var obtainedKeys = getKeys(object, options);
 		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
 	});
 	
 	it('Test for Object with inner Objects', function(){
 		var object = {a : 1, b : 2, c: { l : 4}};
 		var myKeys = ['a', 'b', 'c'];
-		var obtainedKeys = [];
-		getKeys(object, function(err, keys){
-			obtainedKeys = keys;
-		});
+		var obtainedKeys = getKeys(object);
 		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
 	});
 	
 	it('Test for Object with Array', function(){
 		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4}};
 		var myKeys = ['a', 'b', 'c'];
-		var obtainedKeys = [];
-		getKeys(object, function(err, keys){
-			obtainedKeys = keys;
-		});
+		var obtainedKeys = getKeys(object);
 		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
 	});
 	
 	it('Test for Object with inner Object with Array', function(){
 		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4, m : {x : [1,32,45], y : 6}}};
 		var myKeys = ['a', 'b', 'c'];
-		var obtainedKeys = [];
-		getKeys(object, function(err, keys){
-			obtainedKeys = keys;
-		});
+		var obtainedKeys = getKeys(object);
 		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
-	});
-	
-	it('Test with different Order of parameter', function(){
-		assert.throws(function(){
-			getKeys(function(){}, null)}, 'Invalid args');
 	});
 	
 	it('Test with 4 parameter', function(){
@@ -82,10 +71,8 @@ describe('recursive-keys-test', function(){
 		var myKeys = ['a', 'b', 'c', 'l', 'm', 'x', 'y'];
 		var obtainedKeys = [];
 		assert.throws(function(){
-				getKeys(object, options, function(err, keys){
-					obtainedKeys = keys;
-				}, null)
-			}, 'Invalid args'
+				getKeys(object, options, null, null);
+			}, 'Error in Arguments Passed for getKeys()'
 		)
 	});
 	
@@ -97,26 +84,22 @@ describe('recursive-keys-test', function(){
 				getKeys(object, function(err, keys){
 					obtainedKeys = keys;
 				}, options)
-			}, 'Invalid args'
+			}, 'Error in Arguments Passed for getKeys()'
 		)
 	});
 	
 	it('Test with recursive == true', function(){
 		var object = {a : [1, 'a', 5, 'asdf'], b : 2, c: { l : 4, m : {x : [1,32,45], y : 6}}};
 		var myKeys = ['a', 'b', 'c', 'l', 'm', 'x', 'y'];
-		var obtainedKeys = [];
-		getKeys(object, options, function(err, keys){
-			obtainedKeys = keys;
-		});
+		var obtainedKeys = getKeys(object, options);
 		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
 	});
-	
-	it('Test with no callback function as parameter', function(){
-		var object = {a : 1};
-		assert.throws(function(){
-				getKeys(object, options)
-			},'Invalid args'
-		)
+
+	it('Test with Object having inner array having Object or inner array having object', function(){
+		var object = {a : 1, b : [1, {c : [[{d : 2}, {e : 3}], 5]}]};
+		var myKeys = ['a', 'b', 'c', 'd', 'e'];
+		var obtainedKeys = getKeys(object, options);
+		assert.sameMembers(myKeys, obtainedKeys, 'Keys dont match');
 	});
 	
 	after(function(){
