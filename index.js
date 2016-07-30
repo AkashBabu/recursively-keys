c146ef8e1ea41635a5ffa880d4bfaf695d19e059
AkashBabu <001akashbabu@gmail.com>
2016-07-30 13:39:06 +0530

    removed unwanted code

--- index.js
+++ index.js
@@ -15,42 +15,23 @@ var getKeysFunc = function(object, options){
 		if(options.recursive){
 			for(var key in object){
 				keys.push(key);
-				if(object[key].constructor == Object){
+				if(object[key].constructor == Object || object[key].constructor == Array){
 					keys.push(getKeysFunc(object[key], options));
-				}else if(object[key].constructor == Array){
-					object[key].forEach(function(eachElem){
-						if(eachElem.constructor == Object){
-							keys.push(getKeysFunc(object[key], options));
-						}else if(eachElem.constructor == Array){
-							keys.push(getKeysFunc(object[key], options));
-						}
-					})
 				}
 			}
-			first = true;
-			return keys;
 		}else{
-			return Object.keys(object);
+			keys =  Object.keys(object);
 		}
-		return keys;
 	}else if (object.constructor == Array){
 		object.forEach(function(eachElem){
-			if(eachElem.constructor == Object){
+			if(eachElem.constructor == Object || eachElem.constructor == Array){
 				keys.push(getKeysFunc(eachElem, options));
-			}else if(eachElem.constructor == Array){
-				eachElem.forEach(function(eachInnerElem){
-					if(eachInnerElem.constructor == Object){
-						keys.push(getKeysFunc(eachInnerElem, options));
-					}else if(eachInnerElem.constructor == Array){
-						keys.push(getKeysFunc(eachInnerElem, options));
-					}
-				})
 			}
 		})
-		return keys;
 	}else{
 		throw new Error('Error in Arguments Passed for getKeys()');
 	}
+	return keys;
 }
 
 
